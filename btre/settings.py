6685afae4b208ab6ae84af58b563d8d68b9e3ee4
toddcoles <todd_coles@hotmail.com>
2019-09-28 14:38:04 -0700

    Adding static files folder

--- btre/settings.py
+++ btre/settings.py
@@ -23,14 +23,17 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2v$ba0!v(_+=7zvvqqq)gm2@7=jigrcf@1gp84p&ly##4&9m1z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# set to False for PROD
 DEBUG = True
 
+# list of host domains "Digital Ocean"
 ALLOWED_HOSTS = []
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'pages.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'btre.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,4 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT= os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'btre/static')
+]
