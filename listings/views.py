63522d72347005d9a0858a99427ed98d7e2591a5
toddcoles <todd_coles@hotmail.com>
2019-10-08 11:33:48 -0700

    Adding additional models

--- listings/views.py
+++ listings/views.py
@@ -1,15 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from .choices import state_choices, bedroom_choices, price_choices
 from .models import Listing
 
 def index(request):
-  listings = Listing.objects.all()
+  listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+
+  paginator = Paginator(listings, 6)
+  page = request.GET.get('page')
+  paged_listings = paginator.get_page(page)
+
   context = {
-    'listings': listings
+    'listings': paged_listings
   }
   return render(request, 'listings/listings.html', context)
 
 def listing(request, listing_id):
-  return render(request, 'listings/listing.html')
+  listing = get_object_or_404(Listing, pk=listing_id)
+  context = {
+    'listing': listing
+  }
+  return render(request, 'listings/listing.html', context)
 
 def search(request):
-  return render(request, 'listings/search.html')
+  context = {
+    'state_choices': state_choices,
+    'bedroom_choices': bedroom_choices,
+    'price_choices': price_choices
+  }
+  return render(request, 'listings/search.html', context)
