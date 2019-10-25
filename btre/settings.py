b9fe4dacf8a5f6a7980ef526656e02662992e12b
toddcoles <todd_coles@hotmail.com>
2019-10-25 10:25:22 -0700

    Adding search features plus navigation

--- btre/settings.py
+++ btre/settings.py
@@ -136,3 +136,9 @@ STATICFILES_DIRS = [
 # Media folder settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# Messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
